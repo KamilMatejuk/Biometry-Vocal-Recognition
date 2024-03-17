e8277a54d7e2ce41e9d7ec94d228ed62ec9c2212
KamilMatejuk <kamilmatejuk@gmail.com>
2024-03-17 21:53:51 +0100

    define interface

---
+++ models/__init__.py
@@ -0,0 +1,6 @@
+# interface for each model module
+
+# def build(): pass # create instance and load weight if available
+# def train(): pass # finetune model
+# def get_embedding(): pass # return embedding from image
+# def perprocess(): pass # preprocess images
---
+++ models/ghost_face/__init__.py
@@ -0,0 +1,4 @@
+def build(): pass # create instance and load weight if available
+def train(): pass # finetune model
+def get_embedding(): pass # return embedding from image
+def perprocess(): pass # preprocess images
