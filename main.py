aa665e719e7d3975da58e245751ef1eedf97787f
KamilMatejuk <kamilmatejuk@gmail.com>
2024-04-02 21:03:07 +0200

    bs

--- main.py
+++ main.py
@@ -10,9 +10,9 @@ from train import train
 
 
 def action_train(model_name: str, config: dict):
-    dl_train = get_dl_train('data/inputs', device, 32, PreprocessorTrain)
-    dl_test = get_dl_test('data/inputs', device, 32, PreprocessorTest)
-    dl_val = get_dl_val('data/inputs', device, 32, PreprocessorTest)
+    dl_train = get_dl_train('data/inputs', device, config['bs'], PreprocessorTrain)
+    dl_test = get_dl_test('data/inputs', device, config['bs'], PreprocessorTest)
+    dl_val = get_dl_val('data/inputs', device, config['bs'], PreprocessorTest)
     model = Model(device, config)
     train(model, 100, f'{model}/{model_name}', device, dl_train, dl_test, dl_val, None, False)
 
